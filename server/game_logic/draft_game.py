e2cdfd27c006ac85da95fbc555ddc35867bfb8f6
cosmic-explore <nickwactor@gmail.com>
2025-03-05 22:33:13 -0700

    add pub/sub to draft_game

--- server/game_logic/draft_game.py
+++ server/game_logic/draft_game.py
@@ -14,6 +14,7 @@ from connection_util.redis_util import match_from_json, commands_from_json
 redis_connection = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 MATCH_ID = "test_match"
 NULL_STR = "DEV_NULL_STR" # Null values cannot be stored in Redis
+TURN_CHANNEL = "new turn"
 
 def initialize_game(player_1, player_2):
     """Initialize a match and cache it in redis"""
@@ -34,12 +35,12 @@ def submit_command(player_number, command):
         redis_connection.get(f"{MATCH_ID}_2_commands")
     ]
     if all([command != NULL_STR for command in submitted_commands]):
-        adjucate_commands()
+        adjudicate_commands()
     
-def adjucate_commands():
+def adjudicate_commands():
     """Execute the commands, cache the new Match state in redis, reset the
     command cache for the match, and alert players that a new turn has begun"""
-    print("Adjucating!")
+    print("Adjudicating!")
     match = match_from_json(redis_connection.get(MATCH_ID))
     player_1_commands = commands_from_json(redis_connection.get(f"{MATCH_ID}_1_commands"), match)
     player_2_commands = commands_from_json(redis_connection.get(f"{MATCH_ID}_2_commands"), match)
@@ -49,18 +50,25 @@ def adjucate_commands():
     redis_connection.set(f"{MATCH_ID}_1_commands", NULL_STR)
     redis_connection.set(f"{MATCH_ID}_2_commands", NULL_STR)
 
+    redis_connection.publish(TURN_CHANNEL, redis_connection.get(MATCH_ID))
+
 player_1 = get_test_player_1()
 player_2 = get_test_player_2()
 creature_1 = player_1.creatures[0]
 creature_2 = player_2.creatures[0]
 
+turn_listener = redis_connection.pubsub()
+turn_listener.subscribe(TURN_CHANNEL)
 initialize_game(player_1, player_2)
 
 match_json = redis_connection.get(MATCH_ID)
 match = match_from_json(match_json)
+
 submit_command(1, Command(creature_1, match.board[0][9], creature_1.actions[0], creature_2.position))
 submit_command(2, Command(creature_2, match.board[0][0], creature_2.actions[0], creature_1.position))
 
+for message in turn_listener.listen():
+    print(message)
 
 # set creature positions manually
 # creature_1.set_position(board[5][5])
