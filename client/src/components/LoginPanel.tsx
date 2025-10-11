df03bda92ffaca1a4e55c842a3260d32a9442ef1
cosmic-explore <nickwactor@gmail.com>
2025-10-11 14:34:08 +0200

    add disclaimer to login page

--- client/src/components/LoginPanel.tsx
+++ client/src/components/LoginPanel.tsx
@@ -5,16 +5,23 @@ export const LoginPanel = ({ onSubmit }: { onSubmit: Function }) => {
     const [inputText, setInputText] = useState('')
 
     return (
-        <div className="flex">
-            <TextField.Root
-                placeholder="username"
-                className="mr-1"
-                value={inputText}
-                onChange={(event) => setInputText(event.target.value)}
-            />
-            <Button onClick={() => onSubmit(inputText)} style={{ cursor: 'pointer' }}>
-                Login
-            </Button>
+        <div>
+            {' '}
+            <div className="flex">
+                <TextField.Root
+                    placeholder="username"
+                    className="mr-1"
+                    value={inputText}
+                    onChange={(event) => setInputText(event.target.value)}
+                />
+                <Button onClick={() => onSubmit(inputText)} style={{ cursor: 'pointer' }}>
+                    Login
+                </Button>
+            </div>
+            <div className="mt-1">
+                Note: This test version of the game is subject to changes and data-wipes without
+                warning.
+            </div>
         </div>
     )
 }
